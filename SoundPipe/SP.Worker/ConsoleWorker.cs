d5d7ffe6eeacdb482a416c0631da74ff2f96dbf8
Ilep <ilep@noreply.com>
2024-04-06 16:33:05 +0300

    add non-ui usage info

--- SoundPipe/SP.Worker/ConsoleWorker.cs
+++ SoundPipe/SP.Worker/ConsoleWorker.cs
@@ -7,68 +7,67 @@ namespace SP.Worker
     {
         public static void Main()
         {
+            // Приложение SoundPipe может быть также использовано и без UI оболочки
+            // Для этого импортируйте в свое приложение проект SP.Domain
             Console.WriteLine("-= STARTED =-");
 
+            // Регистрируем все необходимые сервисы
             var sp = new ServiceCollection();
             MainModule.RegisterServices(sp);
             var services = sp.BuildServiceProvider();
             var filtersManager = services.GetService(typeof(FiltersManager)) as FiltersManager;
+
+            // Загружаем библиотеки фильтров из указанной директории
+            filtersManager.LoadFilters("C:\\Users\\Eax\\source\\repos\\SoundPipe\\Experiments\\Workspace\\Filters");
+            
+            // Создаем фильтры из которых будет состоять наша схема
+            // В нашем случае это простая цепочка WAV Файл -> Громкость -> Динамик
+            var input = filtersManager.BuildFilter("Input");
+            var volume = filtersManager.BuildFilter("Volume");
+            var speaker = filtersManager.BuildFilter("Speaker");
+
+            // Каждый фильтр инициализируем необходимыми параметрами
+            // Чтобы узнать типы параметров конкретного фильтра можно воспользоваться
+            // Выводом filtersManager.GetFilterMetaInfo("FILTER_NAME").ConstructorParamaters()
+            input.Initialize(new[] { "C:\\Users\\Eax\\Desktop\\MAI\\DIPL\\Experiments\\Workspace\\bwhite11.wav" });
+            volume.Initialize(new object[] { input, 1.0f });
+            speaker.Initialize(new[] { volume });
+
+            // Запускаем работу динамика переведя его в состояние play
+            speaker.SetHotValue("state", "play");
+
+            // Заморозим поток чтобы прослушать звук 3 секунды
+            Thread.Sleep(3000);
+
+            // Теперь поменяем громкость (сделаем ее 2x)
+            volume.SetHotValue("level", 2.0f);
+
+            // Прослушаем - звук стал вдвое громче
+            Thread.Sleep(3000);
+
+            // Высвобождаем задействованные фильтры
+            // Заметим, что первыми нужно высвобождать потребители (например, динамик)
+            speaker.Destroy();
+            volume.Destroy();
+            input.Destroy();
+
+            // Отметим, что SoundPipe можно использовать в множестве сценариев -
+            // Самый простой из них это создание сложных звуковых эффектов для игр
+            // При этом инструмент сам по себе является масштабируемым и производительным
+            Console.WriteLine("-= end =-");
+        }
+    }
+
+    /*
+            Данный закомментированный участок показывает как можно загрузить схему из файла
+            (чтобы не создавать ее непосредственно в коде)
             var shemeManager = services.GetService(typeof(ShemeManager)) as ShemeManager;
             filtersManager.LoadFilters("C:\\Users\\Eax\\source\\repos\\SoundPipe\\Experiments\\Workspace\\Filters\\");
-
             var shemeContent = File.ReadAllText("C:\\Users\\Eax\\Desktop\\MAI\\DIPL\\Experiments\\Workspace\\shemeUIhub.json");
             var shemeConstructor = shemeManager.ReadShemeConstructor<UINodeInfo>(shemeContent);
             var sheme = shemeConstructor.ConstructSheme();
             sheme.SetVar("f3", "state", "play");
-            sheme.SetVar("f2", "state", "play");
             Thread.Sleep(5000);
             sheme.Destroy();
-            
-            //shemeConstructor.SetParameterValue("f2", "level", 2.0f);
-            //shemeConstructor.RemoveFilter("f1");
-            //shemeContent = shemeManager.WriteShemeConstructor(shemeConstructor);
-            //File.WriteAllText("C:\\Users\\Eax\\Desktop\\MAI\\DIPL\\Experiments\\Workspace\\shemeUIout.json", shemeContent);
-            Console.WriteLine("-end-");
-            return;
-            /*var shemeContent = File.ReadAllText("C:\\Users\\Eax\\Desktop\\MAI\\DIPL\\Experiments\\Workspace\\sheme1.json");
-            var sheme = shemeManager.ConstructSheme(shemeContent);
-            sheme.SetVar("f3", "state", "play");
-            for(var i = 0; i < 50; i++)
-            {
-                Console.WriteLine(sheme.GetVar("f4", "level"));
-                Thread.Sleep(100);
-            }
-            sheme.Destroy();
-            Console.WriteLine("-end-");
-            return;*/
-
-            var input = filtersManager.BuildFilter("Input");
-            var volume = filtersManager.BuildFilter("Volume");
-            var speaker = filtersManager.BuildFilter("Speaker");
-            input.Initialize(new[] { "C:\\Users\\Eax\\Desktop\\MAI\\DIPL\\Experiments\\Workspace\\bwhite11.wav" });
-            volume.Initialize(new object[] { input, 1.0f });
-            speaker.Initialize(new[] { volume });
-            Thread.Sleep(3000);
-            for (float i = 1.0f; i < 3.0f; i += 0.1f)
-            {
-                volume.SetHotValue("volume", i);
-                Thread.Sleep(100);
-            }
-            Thread.Sleep(1000);
-            volume.SetHotValue("volume", 1.0f);
-            Thread.Sleep(3000);
-            speaker.Destroy();
-            input.Destroy();
-            Console.WriteLine("-end-");
-            
-            /*
-            
-            Conversions -
-            SPF file -> UI graph
-            UI graph -> SPF file
-            UI graph -> sound processor json file
-
-            */
-        }
-    }
+    */
 }
