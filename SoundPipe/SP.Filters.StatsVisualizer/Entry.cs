f02d9872412acdbcbd99281d952d7b50d51f0734
Ilep <ilep@noreply.com>
2024-04-09 22:10:11 +0300

    'stats' filter added

---
+++ SoundPipe/SP.Filters.StatsVisualizer/Entry.cs
@@ -0,0 +1,64 @@
+﻿using SP.SDK;
+using SP.SDK.Models;
+using SP.SDK.Primitives;
+using SP.SDK.Primitives.Types;
+
+namespace SP.Filters.StatsVisualizer
+{
+    public class Entry : IFilterEntry
+    {
+        public ISoundFilter ConstructFilter()
+        {
+            return new StatsVisualizer();
+        }
+
+        public DynamicParameter[] ConstructorParamaters()
+        {
+            return new DynamicParameter[]
+            {
+                new DynamicParameter("input", "Входной поток", new DynamicStreamType()),
+            };
+        }
+
+        public string FilterDescription()
+        {
+            return "Статистика входного потока";
+        }
+
+        public string FilterName()
+        {
+            return "StatsVisualizer";
+        }
+
+        public int FilterVersion()
+        {
+            return 1;
+        }
+
+        public FilterType GetFilterType()
+        {
+            return FilterType.Provider;
+        }
+
+        public FilterGroupType GetGroupType()
+        {
+            return FilterGroupType.Analyzer;
+        }
+
+        public Uri GetSourceCodeLink()
+        {
+            return new Uri("https://github.com/x64BitWorm/SoundPipe/tree/main/SoundPipe/SP.Filters.StatsVisualizer");
+        }
+
+        public DynamicParameter[] HotParamaters()
+        {
+            return new DynamicParameter[]
+            {
+                new DynamicParameter("totalSamples", "Количество пройденных семплов", new DynamicStringType(25),
+                HotParameterType.ReadOnly),
+                new DynamicParameter("samplesSpeed", "Скорость прохождения семплов", new DynamicStringType(25),
+                HotParameterType.ReadOnly),
+            };
+        }
+    }
+}
---
+++ SoundPipe/SP.Filters.StatsVisualizer/StatsVisualizer.cs
@@ -0,0 +1,90 @@
+﻿using SP.SDK;
+using SP.SDK.Primitives;
+
+namespace SP.Filters.StatsVisualizer
+{
+    public class StatsVisualizer : ISoundFilter
+    {
+        private ISoundProvider _provider;
+        private int _totalSamples;
+        private int _samplesSpeed;
+        private int[] _lastTotalSamples;
+        private Thread _updater;
+
+        public void Initialize(object[] args)
+        {
+            _provider = args[0] as ISoundProvider ?? throw new ArgumentException("Arg1: SoundProvider expected");
+            _totalSamples = 0;
+            _samplesSpeed = 0;
+            _lastTotalSamples = new int[4];
+            _updater = new Thread((e) => UpdateMetrics());
+            _updater.Start();
+        }
+
+        public SoundData ReadPart(int length)
+        {
+            var samples = _provider.ReadPart(length);
+            _totalSamples += samples.Length;
+            return samples;
+        }
+
+        public void SetHotValue(string key, object value)
+        {
+            throw new InvalidOperationException($"STATSVISUALIZER has no SET parameters");
+        }
+
+        public object GetHotValue(string key)
+        {
+            switch (key)
+            {
+                case "totalSamples":
+                    return WithGroups(_totalSamples);
+                case "samplesSpeed":
+                    return WithGroups(_samplesSpeed);
+                default:
+                    throw new InvalidOperationException($"Invalid parameter '{key}'. Only 'totalSamples,samplesSpeed' is supported");
+            }
+        }
+
+        public void Destroy()
+        {
+            _updater.Interrupt();
+        }
+
+        private void UpdateMetrics()
+        {
+            while (true)
+            {
+                for (int i = 0; i < _lastTotalSamples.Length - 1; i++)
+                {
+                    _lastTotalSamples[i] = _lastTotalSamples[i + 1];
+                }
+                _lastTotalSamples[_lastTotalSamples.Length - 1] = _totalSamples;
+                var speed = 0;
+                for (int i = 0; i < _lastTotalSamples.Length - 1; i++)
+                {
+                    speed += _lastTotalSamples[i + 1] - _lastTotalSamples[i];
+                }
+                _samplesSpeed = speed / (_lastTotalSamples.Length - 1);
+                try
+                {
+                    Thread.Sleep(1000);
+                }
+                catch
+                {
+                    break;
+                }
+            }
+        }
+
+        private string WithGroups(int number)
+        {
+            var str = number.ToString();
+            for (int i = str.Length - 3; i > 0; i -= 3)
+            {
+                str = str.Substring(0, i) + ',' + str.Substring(i);
+            }
+            return str;
+        }
+    }
+}
