22f5d38597f07e4cc16056404b212f77d448bf95
Ilep <ilep@noreply.com>
2024-04-09 23:27:55 +0300

    'skip' filter added

---
+++ SoundPipe/SP.Filters.Skip/Entry.cs
@@ -0,0 +1,59 @@
+﻿using SP.SDK;
+using SP.SDK.Models;
+using SP.SDK.Primitives;
+using SP.SDK.Primitives.Types;
+
+namespace SP.Filters.Skip
+{
+    public class Entry : IFilterEntry
+    {
+        public ISoundFilter ConstructFilter()
+        {
+            return new Skip();
+        }
+
+        public DynamicParameter[] ConstructorParamaters()
+        {
+            return new DynamicParameter[]
+            {
+                new DynamicParameter("input", "Входной поток", new DynamicStreamType()),
+                new DynamicParameter("skipSamples", "Количество пропускаемых семплов", new DynamicStringType(8), "0")
+            };
+        }
+
+        public string FilterDescription()
+        {
+            return "Фильтр пропуска начальных семплов в потоке";
+        }
+
+        public string FilterName()
+        {
+            return "Skip";
+        }
+
+        public int FilterVersion()
+        {
+            return 1;
+        }
+
+        public FilterType GetFilterType()
+        {
+            return FilterType.Provider;
+        }
+
+        public FilterGroupType GetGroupType()
+        {
+            return FilterGroupType.Filter;
+        }
+
+        public Uri GetSourceCodeLink()
+        {
+            return new Uri("https://github.com/x64BitWorm/SoundPipe/tree/main/SoundPipe/SP.Filters.Skip");
+        }
+
+        public DynamicParameter[] HotParamaters()
+        {
+            return Array.Empty<DynamicParameter>();
+        }
+    }
+}
---
+++ SoundPipe/SP.Filters.Skip/Skip.cs
@@ -0,0 +1,55 @@
+﻿using SP.SDK;
+using SP.SDK.Primitives;
+
+namespace SP.Filters.Skip
+{
+    public class Skip : ISoundFilter
+    {
+        private ISoundProvider _provider;
+        private int _skip;
+        private bool _skipRequested;
+
+        public void Initialize(object[] args)
+        {
+            _provider = args[0] as ISoundProvider ?? throw new ArgumentException("Arg1: SoundProvider expected");
+            if (args[1] is string skip)
+            {
+                if (!int.TryParse(skip, out _skip))
+                {
+                    _skip = 0;
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Arg2: Int expected");
+            }
+            _skipRequested = true;
+        }
+
+        public SoundData ReadPart(int length)
+        {
+            if (_skipRequested)
+            {
+                _provider.ReadPart(_skip);
+                _skipRequested = false;
+            }
+            var samples = _provider.ReadPart(length);
+            return samples;
+        }
+
+        public void SetHotValue(string key, object value)
+        {
+            throw new InvalidOperationException($"SKIP filter has no SET parameters");
+        }
+
+        public object GetHotValue(string key)
+        {
+            throw new InvalidOperationException($"SKIP filter has no GET parameters");
+        }
+
+        public void Destroy()
+        {
+            // Nothing to do
+        }
+    }
+}
